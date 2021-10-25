51d8fbdd83c55adc3f3c2f6039d255c1405095c8
Hrsikesa108 <c108aravind@gmail.com>
2021-10-25 16:49:54 +0000

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,37 @@
+// import {}
+import './index.css'
+
+const Header = props => {
+  const {score, seconds} = props
+  const ticker = () => {
+    console.log('ticker', seconds)
+  }
+
+  return (
+    <nav className="nav-header">
+      <div className="nav-content">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/match-game-website-logo.png"
+          alt="website logo"
+          className="website-logo"
+        />
+        <ul className="nav-menu">
+          <li>
+            <p className="score">Score:{score}</p>
+          </li>
+          <li className="timer-position">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/match-game-timer-img.png"
+              alt="timer"
+              className="timer"
+            />
+            {/* <p className="score"> {seconds} </p> */}
+            <p className="score"> {seconds} sec</p>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
